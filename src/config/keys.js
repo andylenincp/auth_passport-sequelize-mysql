688d0c9826211aa762413652349cac9893c10d26
Andy Castillo <andy.castillopalma@outlook.com>
2021-05-14 13:19:46 -0500

    make it better

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
-        host: 'remotemysql.com',
-        user: '5JivD3Qzzl',
-        password: 'PpKrhsy6Mh',
-        database: '5JivD3Qzzl'
+        host: 'localhost',
+        user: 'root',
+        password: null,
+        database: 'passport-sequelize-mysql'
     }
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,13 +2,13 @@ const express = require('express');
 const userController = require('../controllers/user');
 const router = express.Router();
 const passport = require('passport');
-const { isLoggedIn } = require('../lib/auth');
+const { isLoggedIn, isNotLoggedIn } = require('../lib/auth');
 
 router.get('/users', isLoggedIn, userController.getUsers);
 router.get('/users/add', isLoggedIn, (req, res) => {
     res.render('users/add');
 });
 router.post('/users/add', userController.createUser);
-router.get('/users/delete/:id', userController.deleteUser);
+router.get('/users/delete/:id', isNotLoggedIn, userController.deleteUser);
 
 module.exports = router;
