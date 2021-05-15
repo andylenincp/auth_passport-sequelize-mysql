7476aa2e39be70f27c1f2b3412661008069d6d7f
Andy Castillo <andy.castillopalma@outlook.com>
2021-05-14 20:15:20 -0500

    db options

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,8 +1,19 @@
 module.exports = {
     database: {
+        host: 'localhost',
+        user: 'root',
+        password: null,
+        database: 'passport-sequelize-mysql'
+    }
+}
+
+/*
+module.exports = {
+    database: {
         host: 'remotemysql.com',
         user: '5JivD3Qzzl',
         password: 'PpKrhsy6Mh',
         database: '5JivD3Qzzl'
     }
 }
+*/
